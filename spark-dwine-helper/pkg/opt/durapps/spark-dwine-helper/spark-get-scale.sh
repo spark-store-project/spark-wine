5c8e0b5fc1fe1e3c475a3098b27485e0cc2c30aa
shenmo <jifengshenmo@outlook.com>
2022-05-30 16:07:34 +0800

    修改：     spark-dwine-helper/pkg/DEBIAN/control 	新文件：   spark-dwine-helper/pkg/opt/deepinwine/tools/spark_run_v4.sh 	修改：     spark-dwine-helper/pkg/opt/durapps/spark-dwine-helper/spark-get-scale.sh

--- spark-dwine-helper/pkg/opt/durapps/spark-dwine-helper/spark-get-scale.sh
+++ spark-dwine-helper/pkg/opt/durapps/spark-dwine-helper/spark-get-scale.sh
@@ -25,8 +25,7 @@ exit
 fi
 
 dimensions=`xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/'`
-
-zenity --list \
+scale_factor=`zenity --list \
 	--width=700 \
 	--height=300 \
        --title="您的分辨率是：$dimensions，请在以下选项中选择一个以运行应用" \
@@ -34,7 +33,16 @@ zenity --list \
        1.0 \
        1.25 \
 	1.5 \
-       2.0
-zenity --info --text="已保存！您可以随时删除~/.config/spark-wine/scale.txt来重新设置"
-echo "$?" > ~/.config/spark-wine/scale.txt
+       2.0`
+
+case "$scale_factor" in 
+	"")
+	zenity --info --text="默认为1倍缩放。您可以随时删除~/.config/spark-wine/scale.txt来重新设置"
+	scale_factor="1.0"
+	;;
+	*)
+zenity --info --text="缩放倍数为$scale_factor。已保存！您可以随时删除~/.config/spark-wine/scale.txt来重新设置" --width=500 --height=150
+	;;
+esac
+echo "$scale_factor" > ~/.config/spark-wine/scale.txt
 
